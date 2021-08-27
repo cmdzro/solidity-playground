7cc6215d51e6eaf3224253ce58195b606f337552
cmdzro <cmdzro@gmail.com>
2021-08-27 11:58:31 +0200

    Add deploy script and configure for ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,30 @@
+const fs = require('fs')
+const path = require('path')
+
 require("@nomiclabs/hardhat-waffle");
+
+const accounts = readJson(`./accounts.json`);
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${accounts.alchemy.apiKey}`,
+      accounts: [`0x${accounts.ropsten.privateKey}`],
+    },
+  },
 };
+
+function readJson(fileName) {
+  let data
+  try {
+    const filePath = path.join(__dirname, fileName)
+    data = fs.readFileSync(filePath)
+  } catch (err) {
+    return null
+  }
+  return JSON.parse(data)
+}
