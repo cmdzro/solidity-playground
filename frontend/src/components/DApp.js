691de7c097ff7e855dc8201568acf434bb842e6b
cmdzro <cmdzro@gmail.com>
2021-08-30 13:34:08 +0200

    Add basic react frontend

---
+++ frontend/src/components/DApp.js
@@ -0,0 +1,23 @@
+import './DApp.css';
+
+function DApp() {
+  return (
+    <div className="DApp">
+      <header className="DApp-header">
+        <p>
+          Edit <code>src/DApp.js</code> and save to reload.
+        </p>
+        <a
+          className="DApp-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default DApp;
