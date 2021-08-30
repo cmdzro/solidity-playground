c52cb780fc0629ad759184198c0ea815a83d5b53
cmdzro <cmdzro@gmail.com>
2021-08-30 15:14:23 +0200

    Apply basic layout for dapp

--- frontend/src/components/DApp.js
+++ frontend/src/components/DApp.js
@@ -6,32 +6,38 @@ import {
 } from "react-router-dom";
 import Home from './Home';
 import BasicToken from './BasicToken';
-import Button from 'react-bootstrap/Button';
 
 function DApp() {
   return (
     <Router>
-      <Button type="Button" class="btn btn-primary">
-        Connect Wallet
-      </Button>
-      <div className="DApp">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/BasicToken">Basic Token</Link>
-          </li>
-        </ul>
+      <div class="container-fluid">
+        <nav class="navbar navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="/">Solidity Playground</a>
+            <form class="d-flex">
+              <button class="btn btn-outline-success me-1" type="button">Connect Wallet</button>
+            </form>
+          </div>
+        </nav>
+        <hr/>
+        <div class="row">
+          <div class="col-2">
+            <nav class="nav flex-column">
+              <Link class="nav-link" href="/BasicToken">Basic Token</Link>
+            </nav>
+          </div>
+          <div class="col">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/BasicToken">
+                <BasicToken />
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/BasicToken">
-          <BasicToken />
-        </Route>
-      </Switch>
     </Router>
   );
 }
