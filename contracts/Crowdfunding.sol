3c5def30d94f7600697bd5c125df4524e2f272be
cmdzro <cmdzro@gmail.com>
2021-09-08 16:54:05 +0200

    Extracting statements into modifier

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -11,6 +11,18 @@ contract Crowdfunding {
   uint256 public goal;
   mapping(address => uint256) public pledgeOf;
 
+  modifier onlyOwner {
+    require(msg.sender == owner,
+            "Not the owner");
+    _;
+  }
+
+  modifier deadlineReached {
+    require(block.timestamp >= deadline,
+            "Funding period not finished yet");
+    _;
+  }
+
   constructor(uint256 _numberOfDays, uint256 _goal) {
     owner = msg.sender;
     deadline = block.timestamp + (_numberOfDays * 1 days);
@@ -27,11 +39,7 @@ contract Crowdfunding {
     emit PledgeCreated(msg.sender, amount);
   }
 
-  function claimFunds() public {
-    require(msg.sender == owner,
-            "Not the owner");
-    require(block.timestamp >= deadline,
-            "Funding period not finished yet");
+  function claimFunds() public onlyOwner deadlineReached {
     require(address(this).balance >= goal,
             "Funding goal missed");
 
@@ -40,12 +48,10 @@ contract Crowdfunding {
     emit FundsClaimed(amount);
   }
 
-  function refund() public {
+  function refund() public deadlineReached {
     uint256 amount = pledgeOf[msg.sender];
 
     // conditions
-    require(block.timestamp >= deadline,
-            "Funding period not finished yet");
     require(address(this).balance < goal,
             "Funding was successfull, nothing to refund");
     require(amount > 0,
