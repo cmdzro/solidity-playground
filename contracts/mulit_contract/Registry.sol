61a9f148966b429585c9a6d0f749380db4bb3270
cmdzro <cmdzro@gmail.com>
2021-10-12 21:45:02 +0200

    Map address and region by deviceId

--- contracts/mulit_contract/Registry.sol
+++ contracts/mulit_contract/Registry.sol
@@ -7,10 +7,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Registry is Ownable {
     mapping(address => uint256) internal deviceId;
     mapping(address => uint8) internal region;
+    mapping(uint256 => address) internal deviceIdToAddress;
 
     function register(uint256 _deviceId, uint8 _region) public {
+        require(_deviceId > 0, "DeviceId can't be 0");
+        require(_region > 0, "Region can't be 0");
+        require(deviceId[msg.sender] == 0, "Already registered");
+        require(deviceIdToAddress[_deviceId] == address(0), "DeviceId already registered");
+
         deviceId[msg.sender] = _deviceId;
         region[msg.sender] = _region;
+        deviceIdToAddress[_deviceId] = msg.sender;
     }
 
     function getDeviceId() external view returns (uint256) {
@@ -19,5 +26,13 @@ contract Registry is Ownable {
 
     function getRegion() external view returns (uint8) {
         return region[msg.sender];
+    }
+
+    function getRegionByDeviceId(uint256 _deviceId) external view returns (uint8) {
+        return region[deviceIdToAddress[_deviceId]];
+    }
+
+    function getAddressByDeviceId(uint256 _deviceId) external view returns (address) {
+        return deviceIdToAddress[_deviceId];
     }
 }
--- test/multi_contract/Registry.js
+++ test/multi_contract/Registry.js
@@ -34,9 +34,38 @@ describe("Registry Contract", () => {
 
         expect(await contract.connect(owner).getDeviceId()).to.equal(0);
         expect(await contract.connect(owner).getRegion()).to.equal(0);
-
+        
         expect(await contract.connect(addr1).getDeviceId()).to.equal(123);
         expect(await contract.connect(addr1).getRegion()).to.equal(1);
-    })
+
+        expect(await contract.connect(owner).getRegionByDeviceId(123)).to.equal(1);
+        expect(await contract.connect(owner).getAddressByDeviceId(123)).to.equal(addr1.address);
+    });
+
+    it("Should fail to register 0 device id", async () => {
+        await expect(contract.register(0, 1))
+          .to.be.revertedWith("DeviceId can't be 0");
+    });
+
+    it("Should fail to register 0 region", async () => {
+        await expect(contract.register(1, 0))
+          .to.be.revertedWith("Region can't be 0");
+    });
   });
+
+  describe("With registration", () => {
+      beforeEach(async () => {
+        await contract.connect(addr1).register(123, 1);
+      });
+
+      it("Should not be able to register again", async () => {
+        await expect(contract.connect(addr1).register(345, 1))
+          .to.be.revertedWith("Already registered");
+      });
+
+      it("Should not be able to register same device id again", async () => {
+          await expect(contract.register(123, 2))
+            .to.be.revertedWith("DeviceId already registered");
+      });
+  })
 });
